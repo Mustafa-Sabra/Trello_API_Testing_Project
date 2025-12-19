fafaf0f0b69b856735ce2dd7604fd4fed5c7e504
Mustafa Sabra <mustafasabra44@gmail.com>
2025-12-19 13:30:37 +0200

    Updating the RestClient class with (post,get,delete, and update) request methods

--- src/test/java/com/automation/client/RestClient.java
+++ src/test/java/com/automation/client/RestClient.java
@@ -2,6 +2,8 @@ package com.automation.client;
 
 import com.automation.base.TestSetup;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,51 +12,121 @@ import static io.restassured.RestAssured.given;
 
 public class RestClient {
 
-    public static Response get(String path, HashMap<String, ?> params) {
+    public static Response get(String path, HashMap<String, ?> params, ResponseSpecification responseSpec) {
+
+        if(responseSpec == null)
+        {
+            throw new IllegalStateException("ResponseSpecification must not be null");
+        }
 
         return  given()
-                .spec(TestSetup.ReqSpecs)
+                .spec(RequestSpecFactory.defaultSpec())
                 .queryParams(params)
+                .log().all()
                 .when()
                 .get(path)
                 .then()
+                .log().all()
+                .spec(responseSpec)
                 .extract().response();
 
     }
 
-    public static Response post(String path, Object body, HashMap<String, ?> params) {
+    public static Response post(String path, Object body, HashMap<String, ?> params, ResponseSpecification responseSpec) {
+
+        if(responseSpec == null)
+        {
+            throw new IllegalStateException("ResponseSpecification must not be null");
+        }
 
         return  given()
-                .spec(TestSetup.ReqSpecs)
+                .spec(RequestSpecFactory.defaultSpec())
                 .queryParams(params)
                 .body(body)
+                .log().all()
                 .when()
                 .post(path)
                 .then()
+                .log().all()
+                .spec(responseSpec)
                 .extract().response();
 
     }
 
-    public static Response delete(String path,String id) {
+    public static Response post(String path , HashMap<String, ?> params, ResponseSpecification responseSpec) {
+
+
+        if (responseSpec == null) {
+            throw new IllegalStateException("ResponseSpecification must not be null");
+        }
 
         return  given()
-                .spec(TestSetup.ReqSpecs)
+                .spec(RequestSpecFactory.defaultSpec())
+                .queryParams(params)
+                .log().all()
                 .when()
-                .delete(path + id)
+                .post(path)
                 .then()
+                .log().all()
+                .spec(responseSpec)
                 .extract().response();
 
     }
 
-    public static Response update(String path,String id,Object body, HashMap<String, ?> params) {
+    public static Response delete(String path, Map<String,?> params, ResponseSpecification responseSpec) {
+
+        if (responseSpec == null) {
+            throw new IllegalStateException("ResponseSpecification must not be null");
+        }
 
         return  given()
-                .spec(TestSetup.ReqSpecs)
+                .spec(RequestSpecFactory.defaultSpec())
+                .queryParams(params)
+                .log().all()
+                .when()
+                .delete(path)
+                .then()
+                .log().all()
+                .spec(responseSpec)
+                .extract().response();
+
+    }
+
+    public static Response update(String path,Object body, HashMap<String, ?> params, ResponseSpecification responseSpec) {
+
+        if (responseSpec == null) {
+            throw new IllegalStateException("ResponseSpecification must not be null");
+        }
+
+        return  given()
+                .spec(RequestSpecFactory.defaultSpec())
                 .queryParams(params)
                 .body(body)
+                .log().all()
                 .when()
-                .put(path + id)
+                .put(path)
                 .then()
+                .log().all()
+                .spec( responseSpec)
+                .extract().response();
+
+    }
+
+    public static Response update(String path, HashMap<String, ?> params, ResponseSpecification responseSpec) {
+
+        if (responseSpec == null) {
+            throw new IllegalStateException("ResponseSpecification must not be null");
+        }
+
+        return  given()
+                .spec(RequestSpecFactory.defaultSpec())
+                .queryParams(params)
+                .log().all()
+                .when()
+                .put(path)
+                .then()
+                .log().all()
+                .spec( responseSpec)
                 .extract().response();
 
     }
