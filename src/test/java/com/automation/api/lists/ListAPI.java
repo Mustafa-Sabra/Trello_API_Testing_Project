f2ec05262c3cf8a45921e457065b14dd05db7afc
Mustafa Sabra <mustafasabra44@gmail.com>
2025-12-19 18:46:43 +0200

    Adding the ListAPI class V0.0

---
+++ src/test/java/com/automation/api/lists/ListAPI.java
@@ -0,0 +1,59 @@
+package com.automation.api.lists;
+
+
+import com.automation.api.boards.CreateBoardParams;
+import com.automation.client.ResponseSpecs;
+import com.automation.client.RestClient;
+import com.automation.utils.AuthUtils;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+
+public class ListAPI   {
+
+    @Step("Create a new List")
+    public Response createList(CreateListParams queryParams)
+    {
+        HashMap<String,Object> params = AuthUtils.authUtils();
+        params.putAll(queryParams.build());
+
+
+        return RestClient.post("/boards", params, ResponseSpecs.created());
+
+    }
+
+    @Step("Get List by ID")
+    public Response getList(String id, HashMap<String,String> queryParams)
+    {
+        HashMap<String,Object> params = AuthUtils.authUtils();
+        params.putAll(queryParams);
+
+
+        return RestClient.post("/boards/" + id, params, ResponseSpecs.created());
+
+    }
+
+    @Step("Archive/Unarchive a List by ID")
+    public Response archiveOrUnarchiveList(String id, HashMap<String,Boolean> queryParams)
+    {
+        HashMap<String,Object> params = AuthUtils.authUtils();
+        params.putAll(queryParams);
+
+
+        return RestClient.post("/boards/" + id, params, ResponseSpecs.created());
+
+    }
+
+    @Step("update List by ID")
+    public Response updateList(String id, HashMap<String,Object> queryParams)
+    {
+        HashMap<String,Object> params = AuthUtils.authUtils();
+        params.putAll(queryParams);
+
+
+        return RestClient.post("/boards/" + id, params, ResponseSpecs.created());
+
+    }
+
+}
