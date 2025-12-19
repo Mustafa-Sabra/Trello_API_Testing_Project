330bdca53a1ccac5eb58abb964d44b942a92543a
Mustafa Sabra <mustafasabra44@gmail.com>
2025-12-19 13:55:33 +0200

    updating the BoardAPI class V0.1

--- src/test/java/com/automation/api/boards/BoardAPI.java
+++ src/test/java/com/automation/api/boards/BoardAPI.java
@@ -1,12 +1,9 @@
-package com.automation.api;
+package com.automation.api.boards;
 
-import com.automation.base.BaseTest;
+import com.automation.client.ResponseSpecs;
 import com.automation.client.RestClient;
-import com.automation.pojo.Board;
 import com.automation.utils.AuthUtils;
 import io.qameta.allure.Step;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 import java.util.HashMap;
@@ -16,32 +13,39 @@ import static io.restassured.RestAssured.given;
 public class BoardAPI  {
 
     @Step("Create a new Board")
-    public Response createBoard(String boardName)
+    public Response createBoard(CreateBoardParams queryParams)
     {
         HashMap<String,String> params = AuthUtils.authUtils();
-        params.put("name", boardName);
+        params.putAll(queryParams.build());
 
 
-        return RestClient.post("/boards", params);
+        return RestClient.post("/boards", params, ResponseSpecs.created());
 
     }
 
     @Step("Get board by ID")
-    public Response getBoard(String id, HashMap<String,String> queryParams)
+    public Response getBoard(String idBoard, HashMap<String,String> queryParams)
     {
-        return RestClient.get("boards" + id, AuthUtils.authUtils());
+        HashMap<String,String> params = AuthUtils.authUtils();
+        params.putAll(queryParams);
+
+        return RestClient.get("/boards/" + idBoard, params, ResponseSpecs.success());
     }
 
     @Step("Delete board by ID")
-    public Response deleteBoard(String id)
+    public Response deleteBoard(String idBoard)
     {
-        return RestClient.delete("/boards" + id, AuthUtils.authUtils());
+        return RestClient.delete("/boards/" + idBoard, AuthUtils.authUtils(), ResponseSpecs.success());
     }
 
     @Step ("Update Board")
-    public Response updateBoard(String id, HashMap <String, String> fields)
+    public Response updateBoard(String idBoard, HashMap <String, String> queryParams)
     {
-        return  null;
+        HashMap<String,String> params = AuthUtils.authUtils();
+        params.putAll(queryParams);
+
+
+        return  RestClient.update("/boards/" + idBoard, params, ResponseSpecs.success());
 
     }
 
