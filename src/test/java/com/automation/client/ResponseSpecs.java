f0312fc426e1c458c59defd132a17afc12bf02d1
Mustafa Sabra <mustafasabra44@gmail.com>
2025-12-19 13:37:48 +0200

    Adding the ResponseSpecs class

---
+++ src/test/java/com/automation/client/ResponseSpecs.java
@@ -0,0 +1,48 @@
+package com.automation.client;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
+
+public class ResponseSpecs {
+
+    public static ResponseSpecification success()
+    {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType("application/json")
+                .build();
+    }
+
+    public static ResponseSpecification created()
+    {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification badRequest()
+    {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(400)
+                .expectContentType("application/json")
+                .build();
+    }
+
+    public static ResponseSpecification unauthorized()
+    {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(401)
+                .expectContentType("application/json")
+                .build();
+    }
+
+    public static ResponseSpecification notFound()
+    {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(404)
+                .expectContentType("application/json")
+                .build();
+    }
+}
