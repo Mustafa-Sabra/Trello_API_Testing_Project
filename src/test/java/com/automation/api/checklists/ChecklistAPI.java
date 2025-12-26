c00623043ba41beb3a1f2a33b6432fa0ba566fae
Mustafa Sabra <mustafasabra44@gmail.com>
2025-12-26 23:08:05 +0200

    update ChecklistAPI class

--- src/test/java/com/automation/api/checklists/ChecklistAPI.java
+++ src/test/java/com/automation/api/checklists/ChecklistAPI.java
@@ -5,42 +5,43 @@ import com.automation.client.RestClient;
 import com.automation.utils.AuthUtils;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
+import io.restassured.specification.ResponseSpecification;
 
 import java.util.HashMap;
 
 public class ChecklistAPI  {
 
     @Step("Create a new Checklist")
-    public Response createChecklist(CreateCheckListParams queryParams)
+    public Response createChecklist(CreateCheckListParams queryParams, ResponseSpecification responseSpecs)
     {
         HashMap<String,Object> params = AuthUtils.authUtils();
         params.putAll(queryParams.build());
 
-        return RestClient.post("/checklists", params, ResponseSpecs.created());
+        return RestClient.post("/checklists", params, responseSpecs);
     }
 
     @Step("Get a Checklist by ID")
-    public Response getChecklist(String id, HashMap<String,String> queryParams)
+    public Response getChecklist(String id, HashMap<String,String> queryParams, ResponseSpecification responseSpecs)
     {
         HashMap<String,Object> params = AuthUtils.authUtils();
         params.putAll(queryParams);
 
-        return RestClient.get("/checklists/" + id, params, ResponseSpecs.success());
+        return RestClient.get("/checklists/" + id, params, responseSpecs);
     }
 
     @Step("Delete a Checklist by ID")
-    public Response deleteChecklist(String id )
+    public Response deleteChecklist(String id, ResponseSpecification responseSpecs )
     {
-        return RestClient.delete("/checklists/" + id, AuthUtils.authUtils(), ResponseSpecs.success());
+        return RestClient.delete("/checklists/" + id, AuthUtils.authUtils(), responseSpecs);
     }
 
     @Step("Update a Checklist by ID")
-    public Response updateChecklist(String id, HashMap<String,Object> queryParams )
+    public Response updateChecklist(String id, HashMap<String,Object> queryParams , ResponseSpecification responseSpecs)
     {
         HashMap<String,Object> params = AuthUtils.authUtils();
         params.putAll(queryParams);
 
-        return RestClient.delete("/checklists/" + id, params, ResponseSpecs.success());
+        return RestClient.update("/checklists/" + id, params, responseSpecs);
     }
 
 
