5a84dd2670b92fc7fd902b8ae02347f67127e369
Mustafa Sabra <mustafasabra44@gmail.com>
2026-01-02 23:02:06 +0200

    Update the TestSetup class

--- src/test/java/com/automation/base/TestSetup.java
+++ src/test/java/com/automation/base/TestSetup.java
@@ -1,16 +1,23 @@
 package com.automation.base;
 
 
+import com.automation.cleanup.CleanupManager;
 import com.automation.utils.ConfigManager;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class TestSetup {
 
-    
+
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite() {
+        CleanupManager.clearAll();
+    }
 
 }
+
